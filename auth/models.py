ae45a899a4c8922c9952a169fed90d1318042ce9
Ed Denham <ed.denham@gmail.com>
2023-02-22 23:33:29 +0000

    Create Profile as extension of AbstractUser

---
+++ auth/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class Profile(AbstractUser):
+    goals = models.JSONField(default=list)
