eecd3c32f57de5b22e1ec7ec4be721330b02282e
Ed Denham <ed.denham@gmail.com>
2023-02-23 00:14:03 +0000

    Add custom user profile to admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile
+from .forms import CustomUserCreationForm
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = Profile()
+    add_form = CustomUserCreationForm()
+
+admin.site.register(Profile, CustomUserAdmin)
