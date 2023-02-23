8fd4d43e0f3054de7653ebb8e9993646851a0308
Ed Denham <ed.denham@gmail.com>
2023-02-23 00:23:46 +0000

    Add labels to custom fields in admin view

--- users/admin.py
+++ users/admin.py
@@ -3,9 +3,26 @@ from .models import Profile
 from .forms import CustomUserCreationForm
 from django.contrib.auth.admin import UserAdmin
 
-
+# https://www.youtube.com/watch?v=1BeZxMbSZNI
 class CustomUserAdmin(UserAdmin):
     model = Profile()
-    add_form = CustomUserCreationForm()
+    add_form = CustomUserCreationForm
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (
+            'Goals',{
+            'fields':(
+                'goals',
+            )
+            }
+        ),         (
+            'Resources',{
+            'fields':(
+                'resources',
+            )
+            }
+        )
+        
+    )
 
 admin.site.register(Profile, CustomUserAdmin)
