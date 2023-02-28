b69dee35ed5f93b13e6e0de04349db7cd1c98eb2
Ed Denham <ed.denham@gmail.com>
2023-02-28 20:58:15 +0000

    Add Dashboard view and url to practicelog

---
+++ practicelog/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('/dashboard', views.Dashboard.as_view(), name='dashboard'),
+    # path('resources', views.Resources.as_view(), name='resources'),
+    # path('log-practice', views.Log_Practice.as_view(), name='log_practice'),
+]
--- practicelog/views.py
+++ practicelog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class Dashboard(View):
+    template_name = "dashboard.html"
+    from django.views import View
+
+    # @login_required
+    def get(self, request):
+        return render(
+            request, 'dashboard.html',
+            {"title": "Dashboard"}
+        ) 
