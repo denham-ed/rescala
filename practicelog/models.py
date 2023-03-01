49ca6231935805a26d2511880442fc4bddb3a0fc
Ed Denham <ed.denham@gmail.com>
2023-03-01 00:25:53 +0000

    Create Session model

--- practicelog/models.py
+++ practicelog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from users.models import Profile
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+
+class Session(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, name='sessions')
+    date = models.DateTimeField()
+    duration = models.IntegerField()
+    headline = models.TextField()
+    image = CloudinaryField('image')
+    focus = models.JSONField(default=list)
+    summary = models.TextField()
+    focus = models.JSONField(default=list)
