19e455b63b2d218081322faeaf08bb9cbe237a1f
Ed Denham <ed.denham@gmail.com>
2023-03-01 00:29:52 +0000

    Add CreateSessionForm

--- practicelog/forms.py
+++ practicelog/forms.py
@@ -1,1 +1,7 @@
-from .models 
+from .models import Session
+from django import forms
+
+class CreateSessionForm(forms.ModelForm):
+    class Meta:
+        model = Session
+        fields = ['date', 'duration', 'headline']
