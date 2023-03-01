60c879ee36ff7f6d6c89b3a4ee4af81c59b3113e
Ed Denham <ed.denham@gmail.com>
2023-03-01 01:01:12 +0000

    Add duration input to create session form

--- practicelog/forms.py
+++ practicelog/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class CreateSessionForm(forms.ModelForm):
     class Meta:
         model = Session
-        fields = ['headline','date']
+        fields = ['headline', 'date', 'duration']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -15,5 +15,9 @@ class CreateSessionForm(forms.ModelForm):
 # https://stackoverflow.com/questions/61076688/django-form-dateinput-with-widget-in-update-loosing-the-initial-value
         self.fields["date"].widget = forms.DateTimeInput(
              attrs={'class': 'form-control ps-5 input-field py-1', 'placeholder': '', 'type': 'date'}
+        )
+
+        self.fields["duration"].widget = forms.NumberInput(
+            attrs={"placeholder": "", "label": "", "class": "form-control input-field py-1", 'type':'number'}
         )
   
