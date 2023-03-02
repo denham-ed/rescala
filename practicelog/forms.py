340402b7263bc589d66e3938dc1ab5539c991bf1
Ed Denham <ed.denham@gmail.com>
2023-03-02 01:15:55 +0000

    Add placeholder range inputs for goals

--- practicelog/forms.py
+++ practicelog/forms.py
@@ -1,6 +1,6 @@
 from .models import Session
 from django import forms
-
+from datetime import datetime
 FOCUS_CHOICES = [
     ('listening', 'Listening'),
     ('scales', 'Scales and Arpeggios'),
@@ -23,23 +23,25 @@ class CreateSessionForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+
         self.fields["headline"].widget = forms.TextInput(
-            attrs={"placeholder": "", "label": "", "class": "input-field py-1"}
+            attrs={"placeholder": "One sentence that describes your practice...", "label": "Add a headline", "class": "input-field py-1"}
         )
 # https://stackoverflow.com/questions/61076688/django-form-dateinput-with-widget-in-update-loosing-the-initial-value
         self.fields["date"].widget = forms.DateTimeInput(
-             attrs={'class': 'form-control ps-5 input-field py-1', 'placeholder': '', 'type': 'date'}
+             attrs={'class': 'form-control input-field py-1', 'placeholder': '', 'type': 'date', 'max':datetime.now().date()}
         )
 
         self.fields["duration"].widget = forms.NumberInput(
             attrs={"placeholder": "", "label": "", "class": "form-control input-field py-1", 'type':'number'}
         )
 
-
         self.fields['focus'] = forms.MultipleChoiceField(choices=FOCUS_CHOICES, widget=forms.CheckboxSelectMultiple(), required=False )
         # self.fields["focus"].widget = forms.CheckboxSelectMultiple(
         #     attrs={"placeholder": "", "label": "", "class": "form-control input-field py-1"}
         # )
+
+        self.fields['summary'].widget=forms.Textarea(attrs={"placeholder":"Reflect on your practice. What went well? What will you work on next time?"})
 
 
   
