f45783e13ed6bf8855ffa27c55d39b6076e5c05b
Ed Denham <ed.denham@gmail.com>
2023-03-02 02:21:37 +0000

    Add sessions as context to dasboard view

--- practicelog/views.py
+++ practicelog/views.py
@@ -1,35 +1,35 @@
-from django.shortcuts import render, reverse
+from django.shortcuts import render, reverse, get_object_or_404
 from django.views import View
 from .forms import CreateSessionForm
 from django.http import HttpResponseRedirect
+from .models import Session
 
-
-# Create your views here.
 
 class Dashboard(View):
     template_name = "dashboard.html"
-    from django.views import View
 
     def get(self, request):
+        sessions = get_object_or_404(Session)
         return render(
-            request, 'dashboard.html'
-        ) 
+                request, 'dashboard.html',
+                {
+                    "sessions": session
+                }
+            ) 
 
 
 class CreateLog(View):
     template_name = "createlog.html"
-    from django.views import View
 
     def get(self, request):
         return render(
             request, 'createlog.html',
             { 
-                
                 "create_session_form": CreateSessionForm()
             }
         )
-    
-    def post(self,request, *args, **kwargs):
+
+    def post(self, request, *args, **kwargs):
         create_session_form = CreateSessionForm(data=request.POST)
         if create_session_form.is_valid():
             session = create_session_form.save(commit=False)
@@ -37,6 +37,9 @@ class CreateLog(View):
             session.save()
             return HttpResponseRedirect(reverse('dashboard'))
         else:
-            return render(request, 'createlog.html',{"create_session_form": CreateSessionForm()})
+            return render(
+                request,
+                'createlog.html',
+                {"create_session_form": CreateSessionForm()}
+            )
 
-        
