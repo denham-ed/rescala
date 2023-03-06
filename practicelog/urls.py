9c457a40dea64b17cb09e6e86443ed5f1a45b2c4
Ed Denham <ed.denham@gmail.com>
2023-03-06 01:26:35 +0000

    Add and wireup delete session button

--- practicelog/urls.py
+++ practicelog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.Dashboard.as_view(), name='dashboard'),
     path('create', views.CreateLog.as_view(), name='create_log'),
-    path('<int:session_id>/', views.SessionDetails.as_view(), name='session_details')
+    path('<int:session_id>/', views.SessionDetails.as_view(), name='session_details'),
+    path('<int:session_id>/delete', views.SessionDetails.delete_session, name='delete_session')
 ]
