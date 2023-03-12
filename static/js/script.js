302fd511343c158877440a52b6903b70a8994302
Ed Denham <ed.denham@gmail.com>
2023-03-12 22:18:17 +0000

    Render focus data as pie chart on dashboard

--- static/js/script.js
+++ static/js/script.js
@@ -13,29 +13,4 @@ $(document).ready(function () {
         $('[data-toggle="tooltip"]').tooltip()
     })
 
-
-    //Chart JS
-    const ctx = document.getElementById('myChart');
-
-    new Chart(ctx, {
-      type: 'pie',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    });
-
-
-
 });
