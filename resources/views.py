fc0c54a7fb582904170c5148cc2cc81d5c9b3f31
Ed Denham <ed.denham@gmail.com>
2023-03-14 04:27:19 +0000

    Render Resource list as cards to resources template

--- resources/views.py
+++ resources/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from .models import Resource
 
 # Create your views here.
 
@@ -7,4 +8,13 @@ class LandingPage(TemplateView):
     template_name = 'landing.html'
 
 class ResourcesPage(TemplateView):
-    template_name = 'resources.html'
+    # template_name = 'resources.html'
+
+    def get(self,request):
+        resources = Resource.objects.all()
+
+        return render(
+            request, 'resources.html',{
+                "resources": resources
+            }
+        )
