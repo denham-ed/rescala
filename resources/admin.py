fed26f5bf50c7358f2cf2d3c59350c9bcd2fafa0
Ed Denham <ed.denham@gmail.com>
2023-03-14 04:01:10 +0000

    Register resource on admin

--- resources/admin.py
+++ resources/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Resource
 
 # Register your models here.
+@admin.register(Resource)
+class ResourceAdmin(admin.ModelAdmin):
+    list_display('title','content','status')
