107a0adc6a1c5769851bb147d3a8d7a841a707ca
Ed Denham <ed.denham@gmail.com>
2023-03-14 20:59:06 +0000

    Install summernote for WYSIWYG control of resource content

--- resources/admin.py
+++ resources/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Resource
 
 # Register your models here.
 @admin.register(Resource)
-class ResourceAdmin(admin.ModelAdmin):
+class ResourceAdmin(SummernoteModelAdmin):
     list_display = ('title', 'content', 'status')
     prepopulated_fields = {'slug': ('title',)}
+    summernote_fields =('content')
