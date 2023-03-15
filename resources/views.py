0c4b35b04ccfb873ca9fc602e23dfd2b2104403c
Ed Denham <ed.denham@gmail.com>
2023-03-15 00:15:56 +0000

    Add cloudinary image to resources

--- resources/views.py
+++ resources/views.py
@@ -8,7 +8,23 @@ from .models import Resource
 class LandingPage(TemplateView):
     template_name = 'landing.html'
 
+
+
+
+
 class ResourcesPage(View):
+
+    # Cloudinary Docs
+    def image_effects(request):
+        return dict(
+            IMAGE_EFFECTS = dict(
+                format = "png",
+                transformation = [
+                    dict(height=95, width=95, crop="thumb", gravity="face", radius=20)
+                ]
+            )
+        )
+
     def get(self, request):
 
         resources = Resource.objects.all()
@@ -17,8 +33,11 @@ class ResourcesPage(View):
         page_number = request.GET.get('page')
         paginated_resources = paginator.get_page(page_number)
 
+        image_effects = self.image_effects()
+
         return render(
             request, 'resources.html',{
-                "resources": paginated_resources
+                "resources": paginated_resources,
+                "IMAGE_EFFECTS": image_effects
             }
         )
