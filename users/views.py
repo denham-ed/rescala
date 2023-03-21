1c1d720576a336e13d002ce25c98cfd9cd020668
Ed Denham <ed.denham@gmail.com>
2023-03-21 02:56:13 +0000

    Redirect user to dashboard if logged in

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 
 
 class LandingPage(View):
     def get(self,request):
+        if request.user.is_authenticated:
+            return redirect('dashboard')
         return render(request, 'landing.html')
