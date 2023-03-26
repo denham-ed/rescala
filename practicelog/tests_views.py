202e334a9f7ab89bd5cf925536e97c0784c02e66
Ed Denham <ed.denham@gmail.com>
2023-03-26 23:15:38 +0000

    Check session context is correct for dashboard view

--- practicelog/tests_views.py
+++ practicelog/tests_views.py
@@ -1,6 +1,9 @@
 from django.test import TestCase, Client
 from users.models import Profile
+from .models import Session
 from django.shortcuts import reverse
+from users.forms import GoalForm
+from datetime import datetime
 
 class TestDashboardView(TestCase):
 
@@ -13,6 +16,24 @@ class TestDashboardView(TestCase):
             username='testuser',
             password='testpass',
         )
+        cls.session1 = Session.objects.create(
+            user= cls.user,
+            headline="Session for Unit Testing",
+            date=datetime.strptime('2023-03-25', '%Y-%m-%d'),
+            duration=30,
+            focus=["rhythm", "sightreading"],
+            moods=["inspired", "determined"],
+            summary="Testing Session",
+        )
+        cls.session2 = Session.objects.create(
+            user= cls.user,
+            headline="Session for Unit Testing",
+            date=datetime.strptime('2023-03-24', '%Y-%m-%d'),
+            duration=30,
+            focus=["rhythm", "sightreading"],
+            moods=["determined"],
+            summary="Testing Session2",
+        )
 
     def test_user_can_view_dashboard(self):
         self.client.login(username='testuser', password='testpass')
@@ -20,7 +41,25 @@ class TestDashboardView(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'dashboard.html')
 
-    # def test_dashboard_context_is_present(self):
+    def test_goals_context(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get(self.url)
+        self.assertEqual(response.context['goals'], self.user.goals)
+        self.assertIsInstance(response.context['goalform'], GoalForm)
+
+
+    def test_sessions_context(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get(self.url)
+        self.assertEqual(list(response.context['sessions']), [self.session1, self.session2])
+
+    def test_sessions_is_empty(self):
+        self.session1.delete()
+        self.session2.delete()
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get(self.url)
+        self.assertQuerysetEqual(response.context['sessions'],[])
+
 
     def test_anonymous_user_is_redirected(self):
         response = self.client.get(self.url)
