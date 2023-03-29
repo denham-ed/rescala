9e727b3e69b6b7cee4a663be241fdaf8b134b777
Ed Denham <ed.denham@gmail.com>
2023-03-29 20:40:20 +0000

    Reformat practice log urls

--- practicelog/urls.py
+++ practicelog/urls.py
@@ -1,13 +1,28 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('', views.Dashboard.as_view(), name='dashboard'),
     path('create', views.CreateLog.as_view(), name='create_log'),
-    path('<int:session_id>/', views.SessionDetails.as_view(), name='session_details'),
-    path('<int:session_id>/edit', views.EditLog.as_view(), name='edit_session'),
-    path('<int:session_id>/delete', views.SessionDetails.delete_session, name='delete_session'),
+    path(
+        '<int:session_id>/',
+        views.SessionDetails.as_view(),
+        name='session_details'),
+    path(
+        '<int:session_id>/edit',
+        views.EditLog.as_view(),
+        name='edit_session'),
+    path(
+        '<int:session_id>/delete',
+        views.SessionDetails.delete_session,
+        name='delete_session'),
     path('goal/add-goal', views.Dashboard.add_goal, name='add_goal'),
-    path('goal/<int:goal_id>/update', views.Dashboard.update_goal, name='update_goal'),
-    path('goal/<int:goal_id>/delete', views.Dashboard.delete_goal, name='delete_goal')
+    path(
+        'goal/<int:goal_id>/update',
+        views.Dashboard.update_goal,
+        name='update_goal'),
+    path(
+        'goal/<int:goal_id>/delete',
+        views.Dashboard.delete_goal,
+        name='delete_goal')
 ]
